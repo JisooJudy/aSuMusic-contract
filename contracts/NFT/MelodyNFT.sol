7135843fb59df9669d516e1ae472e2ff5caa506c
Haneul <>
2023-11-15 14:53:22 +0900

    add LyricsNFT, MelodyNFT

---
+++ contracts/NFT/MelodyNFT.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.9;
+
+import "@openzeppelin/contracts-upgradeable/token/ERC721/ERC721Upgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
+
+// TODO : add interface
+
+contract MelodyNFT is ERC721Upgradeable, OwnableUpgradeable {
+
+}
